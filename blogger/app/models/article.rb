a14a2b8d3eee4f0167b3bc0ba4f0f539a46c282f
CarolTang <carolrtang@gmail.com>
2017-12-17 14:18:57 -0800

    added a few gems

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -4,6 +4,11 @@ class Article < ApplicationRecord
 	has_many :taggings
 	has_many :tags, through: :taggings
 
+	has_many :images
+	# MAY NEED TO FIX LATER--WANT TO BE ABLE TO ATTACH MULTIPLE ATTACHMENTS
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
 	def tag_list
 	# VERSION ONE
 		self.tags.collect do |tag|
